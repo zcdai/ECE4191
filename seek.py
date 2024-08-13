9d7cf5f2f10fe82f8ea1c19470b5cb27ccca69a9
Zhe Dai <zhe.c.dai@gmail.com>
2024-08-13 17:19:25 +1000

    Added new functions to serve MS1

--- seek.py
+++ seek.py
@@ -1,4 +1,4 @@
-from util.robot import DriveCommand
+from util.robot import DriveCommand, PenguinPi
 
 def check_contact(image):
     pass
@@ -7,7 +7,6 @@ def check_contact(image):
 def check_valid_ball(image):
     pass
     # if ball is on screen and close enough to go path to
-
 
 
 def nav2ball(image):
@@ -20,3 +19,10 @@ def nav2ball(image):
 def pickup():
     pass
     # pickup the ball
+
+
+def main():
+    bot = PenguinPi()
+    while True:
+        bot.probe() # search for balls in vicinity
+        bot.primitive_path(bot.ball_pos.pop()) # calculate the primitive path
--- util/robot.py
+++ util/robot.py
@@ -1,6 +1,5 @@
 import numpy as np
-import requests
-import cv2
+
 
 class DriveCommand:
     # forward_speed: [0, 1], float
@@ -9,20 +8,63 @@ class DriveCommand:
         self.forward_speed = forward_speed
         self.turn_speed = turn_speed
 
+class Position:
+    def __init__(self, x, y):
+        # x is length deep, y is length lateral
+        self.x = x
+        self.y = y
+
 
 class PenguinPi:
     def __init__(self):
         self.wheel_vel = [0, 0]
-
-
-
-    def set_velocity(self, drive_cmd: DriveCommand, speed=100, turning_speed=20): 
-        l_vel = drive_cmd.forward_speed*speed - drive_cmd.turn_speed*turning_speed
-        r_vel = drive_cmd.forward_speed*speed + drive_cmd.turn_speed*turning_speed
-        self.wheel_vel = [l_vel, r_vel]
-
-        return l_vel, r_vel
+        self.pos = Position(0, 0)
+        self.angle = 0
+        self.ball_pos = []
 
     def get_image(self):
+        # get image from camera and save positions of any balls
+        # check if balls are a reasonable distance away (< hypotenuse of tennis quadrant)
+        # maybe sort by distance? longest to shortest
         pass
-        # get image from camera and turn it into a processable image
+
+    def get_closest_ball(self):
+        try:
+            return self.ball_pos.pop()
+        except IndexError:
+            return None
+        
+
+
+    def set_angle(self, angle):
+        if angle == self.angle:
+            return
+        angle_delta = angle - self.angle
+        self.rotate(angle_delta)
+
+        self.angle = angle
+
+    def primitive_path(self, new_pos):
+        # calculate the primitive path
+        if new_pos.x > self.pos.x:
+            self.set_angle(0)
+            x_delta = new_pos.x - self.pos.x
+            self.drive(x_delta)
+
+        pass
+
+    def rotate(self, angle):
+        # rotate the robot by angle degrees
+        pass
+
+    def drive(self, distance):
+        # drive forward set distance
+        pass
+
+    def probe(self):
+        # search for balls in vicinity
+        self.get_image()
+
+        while len(self.ball_pos) == 0:
+            self.rotate(45) # TODO: find camera FOV and good rotation value
+            self.get_image()
