31fe473819b19b45df3272fd6c242ba6485de20d
zcdai <zhe.c.dai@gmail.com>
2024-08-14 15:50:32 +1000

    updated main imports

--- seek.py
+++ seek.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Ensure the util directory is in the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'util')))
+from util.robot import BallerRover
+import util.comm as comm
 import util.robot as bot
 
 # def main():
